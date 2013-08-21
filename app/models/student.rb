dcaa7fc58043af0435f0614a40686e169d9fe060
Michael Rhee <mjrhee@gmail.com>
2013-08-21 18:40:00 -0500

    Update student.rb

--- app/models/student.rb
+++ app/models/student.rb
@@ -6,7 +6,7 @@ class Student < ActiveRecord::Base
   validates :email, uniqueness: true
   validates :email, format: { with: /\w+@\w+.\w{2,}/, message: 'invalid email'}
   validates :age, numericality: { greater_than: 5 }
-
+  validate :validate_phone
 
   def name
     self.first_name + ' ' + self.last_name
@@ -18,6 +18,12 @@ class Student < ActiveRecord::Base
     # bday_date = Date.strptime(bday, '%d %b %Y')
     now = Date.today
     ((now - bday_string)/365.25).to_i
+  end
+  
+  def validate_phone
+    unless phone.count('1234567890') == 10                                                                                              = 10
+      errors.add(:base, 'invalid phone number')
+    end
   end
 
 end
